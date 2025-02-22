2f352ebb20290be522f2ce96c71c94792eca7a35
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-02-23 00:01:57 +0700

    chore: import chat sdk core

--- chat_message_page/lib/src/page/message_pages/states/input_state_controller.dart
+++ chat_message_page/lib/src/page/message_pages/states/input_state_controller.dart
@@ -1,5 +1,5 @@
+import 'package:chat_sdk_core/chat_sdk_core.dart';
 import 'package:flutter/material.dart';
-import 'package:v_chat_sdk_core/v_chat_sdk_core.dart';
 
 import '../../../models/input_state_model.dart';
 
