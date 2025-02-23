e6f3ff181ae459c2c2e40eac53470e04cbba7bbe
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-02-23 07:23:27 +0700

    refactor: rename `VCircleAvatar` to `CircleAvatarWidget`
    - Renamed `VCircleAvatar` to `CircleAvatarWidget` in `circle_avatar_widget.dart`.

--- chat_input_ui/lib/src/v_widgets/circle_avatar_widget.dart
+++ chat_input_ui/lib/src/v_widgets/circle_avatar_widget.dart
@@ -1,11 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
-class VCircleAvatar extends StatelessWidget {
+class CircleAvatarWidget extends StatelessWidget {
   final int radius;
   final String fullUrl;
 
-  const VCircleAvatar({
+  const CircleAvatarWidget({
     super.key,
     this.radius = 28,
     required this.fullUrl,
