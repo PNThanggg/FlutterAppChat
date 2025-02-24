918f5fcb7b94e78c9049e019a4d5808f09a44349
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-02-25 04:38:38 +0700

    Refactor: Update `ZTEHomeBadger` to use diamond operator for `ArrayList` creation.

--- app_badger/example/android/ShortcutBadger/src/main/java/com/modules/shortcut_badger/impl/ZTEHomeBadger.java
+++ app_badger/example/android/ShortcutBadger/src/main/java/com/modules/shortcut_badger/impl/ZTEHomeBadger.java
@@ -24,7 +24,7 @@ public class ZTEHomeBadger implements Badger {
 
     @Override
     public List<String> getSupportLaunchers() {
-        return new ArrayList<String>(0);
+        return new ArrayList<>(0);
     }
 } 
 
