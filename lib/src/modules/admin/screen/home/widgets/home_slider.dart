3595f87adfe906040ba3bfccf5f1a417d7c151b4
PNT <92286791+PNThanggg@users.noreply.github.com>
2025-02-25 04:14:39 +0700

    Refactor: Update `home_slider` to use `material` library instead of `cupertino` library.

--- lib/src/modules/admin/screen/home/widgets/home_slider.dart
+++ lib/src/modules/admin/screen/home/widgets/home_slider.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:chat_config/chat_preferences.dart';
 import 'package:chat_core/chat_core.dart';
 import 'package:chat_translation/generated/l10n.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:sidebarx/sidebarx.dart';
